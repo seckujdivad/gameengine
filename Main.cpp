bce07136c43a6a63dd6eb7067ea77732a69352b1
seckujdivad <juckes1david@gmail.com>
2019-11-25 15:19:35 +0000

    Started adding glcanvas elements

--- Main.cpp
+++ Main.cpp
@@ -4,6 +4,15 @@
 Main::Main() : wxFrame(nullptr, wxID_ANY, "Render test", wxPoint(30, 30), wxSize(800, 600))
 {
 	this->SetBackgroundColour("#EEEEEE");
+
+	this->m_sizer = new wxGridBagSizer(0, 0);
+	this->m_sizer->SetFlexibleDirection(wxBOTH);
+	this->m_sizer->SetNonFlexibleGrowMode(wxFLEX_GROWMODE_SPECIFIED);
+
+	this->m_glcanvas = new wxGLCanvas(this, wxID_ANY);
+
+	this->m_sizer->Add(this->m_glcanvas, wxGBPosition(0, 0), wxGBSpan(1, 1), wxEXPAND | wxALL);
+
 }
 
 Main::~Main()
--- Main.h
+++ Main.h
@@ -1,11 +1,15 @@
 #pragma once
 
 #include <wx/wx.h>
+#include <wx/glcanvas.h>
+#include <wx/gbsizer.h>
 
 
 class Main : public wxFrame
 {
 private:
+	wxGridBagSizer* m_sizer;
+	wxGLCanvas* m_glcanvas;
 
 public:
 	Main();
