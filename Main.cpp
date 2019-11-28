53636d44d66672ad90620fc8e7920dc7bc42c73c
seckujdivad <juckes1david@gmail.com>
2019-11-28 22:33:06 +0000

    Bound render button to render function

--- Main.cpp
+++ Main.cpp
@@ -13,7 +13,7 @@ Main::Main() : wxFrame(nullptr, wxID_ANY, "Render test", wxPoint(30, 30), wxSize
 	this->m_sizer->Add(this->m_glcanvas, wxGBPosition(0, 0), wxGBSpan(1, 1), wxEXPAND | wxALL);
 
 	this->m_btn_render = new wxButton(this, wxID_ANY, wxString("Render"));
-	//this->m_btn_render->Bind(wxEVT_BUTTON, &Main::btn_render_OnClick);
+	this->m_btn_render->Bind(wxEVT_BUTTON, &Main::btn_render_OnClick, this);
 	this->m_sizer->Add(this->m_btn_render, wxGBPosition(1, 0), wxGBSpan(1, 1), wxEXPAND | wxALL);
 
 	this->m_sizer->AddGrowableRow(0);
