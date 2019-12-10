f6f2c780b03d25afd30343b043c38d70167fab8c
seckujdivad <juckes1david@gmail.com>
2019-12-10 13:38:32 +0000

    Added headers for Camera

--- src/engine/Camera.cpp
+++ src/engine/Camera.cpp
@@ -1,1 +1,47 @@
 #include "Camera.h"
+
+Camera::Camera()
+{
+
+}
+
+Camera::~Camera()
+{
+
+}
+
+std::tuple<GLfloat, GLfloat, GLfloat>* Camera::GetPosition()
+{
+	return nullptr;
+}
+
+GLfloat Camera::GetPosition(int index)
+{
+	return GLfloat();
+}
+
+void Camera::SetPosition(std::tuple<GLfloat, GLfloat, GLfloat> position)
+{
+}
+
+void Camera::SetPosition(int index, GLfloat value)
+{
+}
+
+std::tuple<GLfloat, GLfloat, GLfloat>* Camera::GetRotation()
+{
+	return nullptr;
+}
+
+GLfloat Camera::GetRotation(int index)
+{
+	return GLfloat();
+}
+
+void Camera::SetRotation(std::tuple<GLfloat, GLfloat, GLfloat> rotation)
+{
+}
+
+void Camera::SetRotation(int index, GLfloat value)
+{
+}
--- src/engine/Camera.h
+++ src/engine/Camera.h
@@ -1,5 +1,25 @@
 #pragma once
+
+#include <wx/glcanvas.h>
+#include <tuple>
+
 class Camera
 {
+private:
+	std::tuple<GLfloat, GLfloat, GLfloat> m_position;
+	std::tuple<GLfloat, GLfloat, GLfloat> m_rotation;
+public:
+	Camera();
+	~Camera();
+
+	std::tuple<GLfloat, GLfloat, GLfloat>* GetPosition();
+	GLfloat GetPosition(int index);
+	void SetPosition(std::tuple<GLfloat, GLfloat, GLfloat> position);
+	void SetPosition(int index, GLfloat value);
+
+	std::tuple<GLfloat, GLfloat, GLfloat>* GetRotation();
+	GLfloat GetRotation(int index);
+	void SetRotation(std::tuple<GLfloat, GLfloat, GLfloat> rotation);
+	void SetRotation(int index, GLfloat value);
 };
 
