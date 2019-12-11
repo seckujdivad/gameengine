2c58a8125fddd407a967e2d226549c1f085437c4
seckujdivad <juckes1david@gmail.com>
2019-12-11 14:48:58 +0000

    Wrote getters and setters for scene class

--- src/engine/Scene.cpp
+++ src/engine/Scene.cpp
@@ -1,11 +1,64 @@
 #include "Scene.h"
 
+int Scene::GetModelIndex(Model* model)
+{
+	for (int i = 0; i < this->m_models.size(); i++)
+	{
+		if (model == this->m_models.at(i))
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+
+int Scene::GetCameraIndex(Camera* camera)
+{
+	for (int i = 0; i < this->m_cameras.size(); i++)
+	{
+		if (camera == this->m_cameras.at(i))
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+
 Scene::Scene()
 {
 }
 
 Scene::~Scene()
 {
+}
+
+void Scene::AddModel(Model* model)
+{
+	this->m_models.push_back(model);
+}
+
+void Scene::RemoveModel(Model* model)
+{
+	this->m_models.erase(this->m_models.begin() + this->GetModelIndex(model));
+}
+
+void Scene::AddCamera(Camera* camera)
+{
+	this->m_cameras.push_back(camera);
+}
+
+void Scene::RemoveCamera(Camera* camera)
+{
+}
+
+void Scene::SetActiveCamera(Camera* camera)
+{
+	this->m_active_camera = camera;
+}
+
+Camera* Scene::GetActiveCamera()
+{
+	return this->m_active_camera;
 }
 
 void Scene::Render(EngineCanvas* canvas)
--- src/engine/Scene.h
+++ src/engine/Scene.h
@@ -3,16 +3,30 @@
 #include <vector>
 
 #include "Model.h"
+#include "Camera.h"
 #include "render/EngineCanvas.h"
 
 class Scene
 {
 private:
-	std::vector<Model> m_models;
+	std::vector<Model*> m_models;
+	std::vector<Camera*> m_cameras;
+
+	Camera* m_active_camera;
+
+	int GetModelIndex(Model* model);
+	int GetCameraIndex(Camera* camera);
 	
 public:
 	Scene();
 	~Scene();
+
+	void AddModel(Model* model);
+	void RemoveModel(Model* model);
+	void AddCamera(Camera* camera);
+	void RemoveCamera(Camera* camera);
+	void SetActiveCamera(Camera* camera);
+	Camera* GetActiveCamera();
 
 	void Render(EngineCanvas* canvas);
 };
