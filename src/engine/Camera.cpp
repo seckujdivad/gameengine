47b1655fc0f495d3058a6190f3b491366d39e7f1
seckujdivad <juckes1david@gmail.com>
2019-12-16 11:00:51 +0000

    Wrote more methods for changing Camera data

--- src/engine/Camera.cpp
+++ src/engine/Camera.cpp
@@ -4,12 +4,35 @@ Camera::Camera()
 {
 	this->m_position = new std::array<GLfloat, 3>;
 	this->m_rotation = new std::array<GLfloat, 3>;
+
+	this->m_identifier = "";
+	this->m_fov = (GLfloat)45;
 }
 
 Camera::~Camera()
 {
 	delete this->m_position;
 	delete this->m_rotation;
+}
+
+void Camera::SetIdentifier(std::string identifier)
+{
+	this->m_identifier = identifier;
+}
+
+std::string Camera::GetIdentifier()
+{
+	return this->m_identifier;
+}
+
+void Camera::SetFOV(GLfloat fov)
+{
+	this->m_fov = fov;
+}
+
+GLfloat Camera::GetFOV()
+{
+	return this->m_fov;
 }
 
 std::array<GLfloat, 3> Camera::GetPosition()
@@ -35,6 +58,13 @@ void Camera::SetPosition(int index, GLfloat value)
 	this->m_position->at(index) = value;
 }
 
+void Camera::SetPosition(GLfloat x, GLfloat y, GLfloat z)
+{
+	this->m_position->at(0) = x;
+	this->m_position->at(1) = y;
+	this->m_position->at(2) = z;
+}
+
 std::array<GLfloat, 3> Camera::GetRotation()
 {
 	return *this->m_rotation;
@@ -56,4 +86,11 @@ void Camera::SetRotation(std::array<GLfloat, 3> rotation)
 void Camera::SetRotation(int index, GLfloat value)
 {
 	this->m_rotation->at(index) = value;
+}
+
+void Camera::SetRotation(GLfloat x_rot, GLfloat y_rot, GLfloat z_rot)
+{
+	this->m_rotation->at(0) = x_rot;
+	this->m_rotation->at(1) = y_rot;
+	this->m_rotation->at(2) = z_rot;
 }
--- src/engine/Camera.h
+++ src/engine/Camera.h
@@ -1,25 +1,38 @@
 #pragma once
 
 #include <wx/glcanvas.h>
+
 #include <array>
+#include <string>
 
 class Camera
 {
 private:
 	std::array<GLfloat, 3>* m_position;
 	std::array<GLfloat, 3>* m_rotation;
+
+	std::string m_identifier;
+	GLfloat m_fov;
+
 public:
 	Camera();
 	~Camera();
+
+	void SetIdentifier(std::string identifier);
+	std::string GetIdentifier();
+
+	void SetFOV(GLfloat fov);
+	GLfloat GetFOV();
 
 	std::array<GLfloat, 3> GetPosition();
 	GLfloat GetPosition(int index);
 	void SetPosition(std::array<GLfloat, 3> position);
 	void SetPosition(int index, GLfloat value);
+	void SetPosition(GLfloat x, GLfloat y, GLfloat z);
 
 	std::array<GLfloat, 3> GetRotation();
 	GLfloat GetRotation(int index);
 	void SetRotation(std::array<GLfloat, 3> rotation);
 	void SetRotation(int index, GLfloat value);
+	void SetRotation(GLfloat x_rot, GLfloat y_rot, GLfloat z_rot);
 };
-
