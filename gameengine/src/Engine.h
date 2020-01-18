a0f4b60cf2e0745d8dc3678d7dbd956acd084632
seckujdivad <juckes1david@gmail.com>
2020-01-18 16:52:04 +0000

    Wrote PLY header loading

--- gameengine/src/Engine.h
+++ gameengine/src/Engine.h
@@ -10,7 +10,6 @@
 #include <vector>
 #include <map>
 #include <tuple>
-#include <regex>
 
 #include "Scene.h"
 #include "Model.h"
@@ -18,14 +17,31 @@
 
 using nlohmann::json;
 
-Scene* InitialiseScene(std::string path, std::string filename);
+struct PlyType
+{
+	bool is_list;
+	bool is_ints;
+};
 
 struct PlyElement
 {
 	std::string name;
 	int num_elements;
-	std::vector<std::string> types;
+	std::vector<PlyType> types;
 	std::vector<std::string> field_names;
 };
 
+struct PlyValueList
+{
+	bool is_ints;
+	std::vector<double> values_double;
+	std::vector<int> values_ints;
+};
+
+Scene* InitialiseScene(std::string path, std::string filename);
+
 Model* ModelFromPly(std::string path);
+bool IsPlyInt(std::string type_name);
+
+std::vector<std::string> SplitOnChar(std::string string, char splitter, bool add_empty = false);
+std::vector<std::string> SplitOnChar(std::string string, std::string splitter, bool add_empty = false);
