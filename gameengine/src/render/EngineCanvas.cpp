4dd0a82062fc2932b9d4634b6fbe5cac8076006e
seckujdivad <juckes1david@gmail.com>
2020-01-21 19:06:07 +0000

    Fixed shader compilation

--- gameengine/src/render/EngineCanvas.cpp
+++ gameengine/src/render/EngineCanvas.cpp
@@ -13,12 +13,64 @@ EngineCanvas::EngineCanvas(wxWindow* parent, wxWindowID id, const int* args) : w
 	glLoadIdentity();
 	glewInit();
 
+	int success;
+	char infoLog[512];
+	int msglen;
+
 	//verts
 	float verts[] = {
 		-0.5f, -0.5f, 0.0f,
 		0.5f, -0.5f, 0.0f,
 		0.0f, 0.5f, 0.0f
 	};
+
+	//vert shader
+	const char* vertex_shader_src = "#version 400 core\n"
+		"layout (location = 0) in vec3 aPos;\n"
+		"void main()\n"
+		"{\n"
+		"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0f);\n"
+		"}\n";
+
+	unsigned int vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertex_shader, 1, &vertex_shader_src, NULL);
+	glCompileShader(vertex_shader);
+
+	//frag shader
+	const char* frag_shader_src = "#version 400 core\n"
+		"out vec4 gl_FragColor;\n"
+		"void main()\n"
+		"{\n"
+		"	gl_FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+		"}\n";
+
+	unsigned int frag_shader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(frag_shader, 1, &frag_shader_src, NULL);
+	glCompileShader(frag_shader);
+
+	glGetShaderiv(frag_shader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(frag_shader, 512, NULL, infoLog);
+
+	}
+
+	//shader program
+	this->m_shader_program = glCreateProgram();
+	glAttachShader(this->m_shader_program, vertex_shader);
+	glAttachShader(this->m_shader_program, frag_shader);
+	glLinkProgram(this->m_shader_program);
+	glUseProgram(this->m_shader_program);
+
+	// check for linking errors
+	
+	std::string k;
+	glGetProgramiv(this->m_shader_program, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(this->m_shader_program, sizeof(infoLog), &msglen, infoLog);
+		k = std::string(infoLog);
+	}
+	k;
 
 	unsigned int* vao = &this->m_VAO;
 	glGenVertexArrays(1, vao);
@@ -30,40 +82,8 @@ EngineCanvas::EngineCanvas(wxWindow* parent, wxWindowID id, const int* args) : w
 	glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
 
-	//vert shader
-	const char* vertex_shader_src = "#version 400 core\n"
-		"layout (location = 0) in vec3 aPos;\n"
-		"void main()\n"
-		"{\n"
-		"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-		"}\n";
-
-	unsigned int vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertex_shader, 1, &vertex_shader_src, NULL);
-	glCompileShader(vertex_shader);
-
-	//frag shader
-	const char* frag_shader_src = "#version 400 core\n"
-		"out vec4 FragColour;\n"
-		"void main()\n"
-		"{\n"
-		"	FragColour = vec4(1.0f, 0.5f, 0.2f, 1.0);\n"
-		"}\n";
-
-	unsigned int frag_shader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(frag_shader, 1, &frag_shader_src, NULL);
-	glCompileShader(frag_shader);
-
-	//shader program
-	this->m_shader_program = glCreateProgram();
-	glAttachShader(this->m_shader_program, vertex_shader);
-	glAttachShader(this->m_shader_program, frag_shader);
-	glUseProgram(this->m_shader_program);
-
 	glDeleteShader(vertex_shader);
 	glDeleteShader(frag_shader);
-
-	
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
