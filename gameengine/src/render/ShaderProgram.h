b0441e297f7c88d5dd762ff001dd7714d97f526d
seckujdivad <juckes1david@gmail.com>
2020-01-21 22:36:26 +0000

    Wrote header for ShaderProgram class

--- gameengine/src/render/ShaderProgram.h
+++ gameengine/src/render/ShaderProgram.h
@@ -8,4 +8,14 @@ class ShaderProgram
 {
 private:
 public:
+	GLuint program_id;
+	
+	ShaderProgram(std::string vert_path, std::string frag_path, bool throw_errors = true);
+	~ShaderProgram();
+
+	void Select();
+
+	void SetUniform(std::string name, bool value);
+	void SetUniform(std::string name, int value);
+	void SetUniform(std::string name, float value);
 };
