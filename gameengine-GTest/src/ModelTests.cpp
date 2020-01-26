4a380e24090b5555aa0d2f0b9fbfc38911764b29
seckujdivad <juckes1david@gmail.com>
2020-01-26 23:08:33 +0000

    Added vertex creation test

--- gameengine-GTest/src/ModelTests.cpp
+++ gameengine-GTest/src/ModelTests.cpp
@@ -1,11 +1,17 @@
 #include "pch.h"
+
 #include "Model.h"
 
-TEST(ModelTests, Identifier_GetSet)
+TEST(ModelTests, Vertex_SetGet_Vector)
 {
 	Model* mdl = new Model();
-	EXPECT_EQ(mdl->GetIdentifier(), "");
+	
+	std::vector<GLfloat> vertices = { 0.1f, 3.5f, -9.0f };
+	int index = mdl->AddVertex(vertices);
+	std::vector<GLfloat>* vertices_result = mdl->GetVertex(index);
 
-	mdl->SetIdentifier("Hello World!");
-	EXPECT_EQ(mdl->GetIdentifier(), "Hello World!");
+	for (size_t i = 0; i < 3; i++)
+	{
+		EXPECT_EQ(vertices_result->at(i), vertices.at(i));
+	}
 }
