bf55946a75b21edba94210323ab0ebaed5ebf73e
seckujdivad <juckes1david@gmail.com>
2020-01-26 23:00:36 +0000

    Changed Camera to inherit from Entity

--- gameengine/src/Camera.cpp
+++ gameengine/src/Camera.cpp
@@ -1,29 +1,14 @@
 #include <wx/wxprec.h>
 #include "Camera.h"
 
-Camera::Camera()
+Camera::Camera() : Entity()
 {
-	this->m_position = new std::array<GLfloat, 3>;
-	this->m_rotation = new std::array<GLfloat, 3>;
-
-	this->m_identifier = "";
 	this->m_fov = (GLfloat)45;
 }
 
 Camera::~Camera()
 {
-	delete this->m_position;
-	delete this->m_rotation;
-}
 
-void Camera::SetIdentifier(std::string identifier)
-{
-	this->m_identifier = identifier;
-}
-
-std::string Camera::GetIdentifier()
-{
-	return this->m_identifier;
 }
 
 void Camera::SetFOV(GLfloat fov)
@@ -34,64 +19,4 @@ void Camera::SetFOV(GLfloat fov)
 GLfloat Camera::GetFOV()
 {
 	return this->m_fov;
-}
-
-std::array<GLfloat, 3> Camera::GetPosition()
-{
-	return *this->m_position;
-}
-
-GLfloat Camera::GetPosition(int index)
-{
-	return this->m_position->at(index);
-}
-
-void Camera::SetPosition(std::array<GLfloat, 3> position)
-{
-	for (int i = 0; i < 3; i++)
-	{
-		this->m_position->at(i) = position.at(i);
-	}
-}
-
-void Camera::SetPosition(int index, GLfloat value)
-{
-	this->m_position->at(index) = value;
-}
-
-void Camera::SetPosition(GLfloat x, GLfloat y, GLfloat z)
-{
-	this->m_position->at(0) = x;
-	this->m_position->at(1) = y;
-	this->m_position->at(2) = z;
-}
-
-std::array<GLfloat, 3> Camera::GetRotation()
-{
-	return *this->m_rotation;
-}
-
-GLfloat Camera::GetRotation(int index)
-{
-	return this->m_rotation->at(index);
-}
-
-void Camera::SetRotation(std::array<GLfloat, 3> rotation)
-{
-	for (int i = 0; i < 3; i++)
-	{
-		this->m_rotation->at(i) = rotation.at(i);
-	}
-}
-
-void Camera::SetRotation(int index, GLfloat value)
-{
-	this->m_rotation->at(index) = value;
-}
-
-void Camera::SetRotation(GLfloat x_rot, GLfloat y_rot, GLfloat z_rot)
-{
-	this->m_rotation->at(0) = x_rot;
-	this->m_rotation->at(1) = y_rot;
-	this->m_rotation->at(2) = z_rot;
 }
--- gameengine/src/Camera.h
+++ gameengine/src/Camera.h
@@ -6,13 +6,12 @@
 #include <array>
 #include <string>
 
+#include "Entity.h"
 
-class Camera
+
+class Camera : public Entity
 {
 private:
-	std::array<GLfloat, 3>* m_position;
-	std::array<GLfloat, 3>* m_rotation;
-
 	std::string m_identifier;
 	GLfloat m_fov;
 
@@ -20,21 +19,6 @@ public:
 	Camera();
 	~Camera();
 
-	void SetIdentifier(std::string identifier);
-	std::string GetIdentifier();
-
 	void SetFOV(GLfloat fov);
 	GLfloat GetFOV();
-
-	std::array<GLfloat, 3> GetPosition();
-	GLfloat GetPosition(int index);
-	void SetPosition(std::array<GLfloat, 3> position);
-	void SetPosition(int index, GLfloat value);
-	void SetPosition(GLfloat x, GLfloat y, GLfloat z);
-
-	std::array<GLfloat, 3> GetRotation();
-	GLfloat GetRotation(int index);
-	void SetRotation(std::array<GLfloat, 3> rotation);
-	void SetRotation(int index, GLfloat value);
-	void SetRotation(GLfloat x_rot, GLfloat y_rot, GLfloat z_rot);
 };
