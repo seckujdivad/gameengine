695be96013b12436521d3b1d67d25c58794c4028
seckujdivad <juckes1david@gmail.com>
2020-01-27 20:29:46 +0000

    Wrote Camera OpenGL matrix generation

--- gameengine/src/Camera.cpp
+++ gameengine/src/Camera.cpp
@@ -4,6 +4,9 @@
 Camera::Camera() : Entity()
 {
 	this->m_fov = (GLfloat)45;
+
+	this->perspective_matrix = glm::mat4(1.0f);
+	this->view_matrix = glm::mat4(1.0f);
 }
 
 Camera::~Camera()
@@ -19,4 +22,38 @@ void Camera::SetFOV(GLfloat fov)
 GLfloat Camera::GetFOV()
 {
 	return this->m_fov;
+}
+
+void Camera::SetNearClip(GLfloat nearclip)
+{
+	this->m_clip_near = nearclip;
+}
+
+GLfloat Camera::GetNearClip()
+{
+	return this->m_clip_near;
+}
+
+void Camera::SetFarClip(GLfloat farclip)
+{
+	this->m_clip_far = farclip;
+}
+
+GLfloat Camera::GetFarClip()
+{
+	return this->m_clip_far;
+}
+
+void Camera::GenViewMat()
+{
+	this->view_matrix = glm::mat4(1.0f);
+	this->view_matrix = glm::translate(this->view_matrix, glm::vec3(0 - this->GetPosition(0), 0 - this->GetPosition(1), 0 - this->GetPosition(2)));
+	this->view_matrix = glm::rotate(this->view_matrix, glm::radians(0 - this->GetRotation(0)), glm::vec3(1.0f, 0.0f, 0.0f));
+	this->view_matrix = glm::rotate(this->view_matrix, glm::radians(0 - this->GetRotation(1)), glm::vec3(0.0f, 1.0f, 0.0f));
+	this->view_matrix = glm::rotate(this->view_matrix, glm::radians(0 - this->GetRotation(2)), glm::vec3(0.0f, 0.0f, 1.0f));
+}
+
+void Camera::GenPerspMat(float window_width, float window_height)
+{
+	this->perspective_matrix = glm::perspective(glm::radians(this->m_fov), window_width / window_height, this->m_clip_near, this->m_clip_far);
 }
--- gameengine/src/Camera.h
+++ gameengine/src/Camera.h
@@ -3,22 +3,39 @@
 #include <wx/wxprec.h>
 #include "GLComponents.h"
 
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+
 #include <array>
 #include <string>
 
 #include "Entity.h"
 
-
 class Camera : public Entity
 {
 private:
 	std::string m_identifier;
-	GLfloat m_fov;
+	GLfloat m_fov = 45.0f;
+
+	GLfloat m_clip_near = 0.1f;
+	GLfloat m_clip_far = 100.0f;
 
 public:
+	glm::mat4 view_matrix;
+	glm::mat4 perspective_matrix;
+
 	Camera();
 	~Camera();
 
 	void SetFOV(GLfloat fov);
 	GLfloat GetFOV();
+
+	void SetNearClip(GLfloat nearclip);
+	GLfloat GetNearClip();
+
+	void SetFarClip(GLfloat farclip);
+	GLfloat GetFarClip();
+
+	void GenViewMat();
+	void GenPerspMat(float window_width, float window_height);
 };
