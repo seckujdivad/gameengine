64712515c0544798097419328512eb0c44b1550c
seckujdivad <juckes1david@gmail.com>
2020-02-16 17:14:37 +0000

    Added spinctrl to spin the current model along the z-axis

--- demo-cube/src/Main.cpp
+++ demo-cube/src/Main.cpp
@@ -24,14 +24,21 @@ Main::Main() : wxFrame(nullptr, wxID_ANY, "Render test", wxPoint(30, 30), wxSize
 
 	this->m_glcanvas = new EngineCanvas(this, wxID_ANY, gl_args);
 
-	this->m_sizer->Add(this->m_glcanvas, wxGBPosition(0, 0), wxGBSpan(1, 1), wxEXPAND | wxALL);
+	this->m_sizer->Add(this->m_glcanvas, wxGBPosition(0, 0), wxGBSpan(1, 2), wxEXPAND | wxALL);
 
 	this->m_btn_render = new wxButton(this, wxID_ANY, wxString("Render"));
 	this->m_btn_render->Bind(wxEVT_BUTTON, &Main::btn_render_OnClick, this);
 	this->m_sizer->Add(this->m_btn_render, wxGBPosition(1, 0), wxGBSpan(1, 1), wxEXPAND | wxALL);
 
+	this->m_spn_zrot = new wxSpinCtrl(this, wxID_ANY);
+	this->m_spn_zrot->SetMax(999);
+	this->m_spn_zrot->SetMin(-999);
+	this->m_spn_zrot->Bind(wxEVT_SPINCTRL, &Main::spn_zrot_OnChange, this);
+	this->m_sizer->Add(this->m_spn_zrot, wxGBPosition(1, 1), wxGBSpan(1, 1), wxEXPAND | wxALL);
+
 	this->m_sizer->AddGrowableRow(0);
 	this->m_sizer->AddGrowableCol(0);
+	this->m_sizer->AddGrowableCol(1);
 
 	//load scene
 #ifndef GE_USESAMPLE
@@ -54,6 +61,13 @@ Main::~Main()
 
 void Main::btn_render_OnClick(wxCommandEvent& evt)
 {
+	this->m_glcanvas->Render();
+	evt.Skip();
+}
+
+void Main::spn_zrot_OnChange(wxCommandEvent& evt)
+{
+	this->m_scene->models.at(0)->SetRotation(2, this->m_spn_zrot->GetValue());
 	this->m_glcanvas->Render();
 	evt.Skip();
 }
--- demo-cube/src/Main.h
+++ demo-cube/src/Main.h
@@ -3,6 +3,7 @@
 #include <wx/wxprec.h>
 #include <wx/wx.h>
 #include <wx/gbsizer.h>
+#include <wx/spinctrl.h>
 
 #include "GLComponents.h"
 #include "render/EngineCanvas.h"
@@ -14,10 +15,12 @@ private:
 	wxGridBagSizer* m_sizer;
 	EngineCanvas* m_glcanvas;
 	wxButton* m_btn_render;
+	wxSpinCtrl* m_spn_zrot;
 
 	Scene* m_scene;
 
 	void btn_render_OnClick(wxCommandEvent& evt);
+	void spn_zrot_OnChange(wxCommandEvent& evt);
 
 public:
 	Main();
