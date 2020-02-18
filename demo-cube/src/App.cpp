92b8d1e2703fda7010c37d4dca7d47841ad94859
seckujdivad <juckes1david@gmail.com>
2020-02-18 19:01:24 +0000

    Gave pause message box some explanation text

--- demo-cube/src/App.cpp
+++ demo-cube/src/App.cpp
@@ -5,7 +5,8 @@ wxIMPLEMENT_APP(App);
 
 bool App::OnInit()
 {
-	wxMessageBox("");
+	wxMessageBox("Inject debuggers (RenderDoc etc) if you are using them into this process before clicking OK", "Inject debuggers");
+
 	this->m_frame_main = new Main();
 	this->m_frame_main->Show();
 
