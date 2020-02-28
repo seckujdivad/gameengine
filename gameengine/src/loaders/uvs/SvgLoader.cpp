fa322b7af0b2549043bb1545e1ec5821c9f11c94
seckujdivad <juckes1david@gmail.com>
2020-02-28 22:24:57 +0000

    Added more UV data

---
+++ gameengine/src/loaders/uvs/SvgLoader.cpp
@@ -0,0 +1,13 @@
+#include <wx/wxprec.h>
+#include "SvgLoader.h"
+
+void UVFromSvg(Model* model, std::string path)
+{
+	wxXmlDocument xml_doc;
+	if (!xml_doc.Load(path))
+	{
+		throw std::runtime_error("Couldn't open file '" + path + '"');
+	}
+
+	if 
+}
---
+++ gameengine/src/loaders/uvs/SvgLoader.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <wx/wxprec.h>
+
+#include <wx/xml/xml.h>
+
+#include <string>
+
+#include "../../Model.h"
+
+void UVFromSvg(Model* model, std::string path);
