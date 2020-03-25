44364eb089ce2208a3f3af62e8a7e26373a2d0d6
seckujdivad <juckes1david@gmail.com>
2020-03-25 16:21:39 +0000

    Fixed UV map loading

--- gameengine/src/loaders/models/PlyLoader.cpp
+++ gameengine/src/loaders/models/PlyLoader.cpp
@@ -134,7 +134,7 @@ Model* ModelFromPly(std::string path)
 						));
 						vertex_uvs.push_back(glm::vec2(
 							(GLfloat)std::stof(sliced_string.at(header_layout.at(pattern_index)->field_name_map.at("s"))),
-							(GLfloat)std::stof(sliced_string.at(header_layout.at(pattern_index)->field_name_map.at("t")))
+							0.0f - (GLfloat)std::stof(sliced_string.at(header_layout.at(pattern_index)->field_name_map.at("t"))) //switch from top left (images) to bottom left (opengl) coordinate system
 						));
 					}
 					else if (header_layout.at(pattern_index)->name == "face")
