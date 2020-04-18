f0084fe9f2c97ab547a48484d56f4bef14c440ad
seckujdivad <juckes1david@gmail.com>
2020-04-18 23:25:14 +0100

    Fixed default sizing of floating panes to be the frame's best size

--- gameengine-editor/src/PaneHost.cpp
+++ gameengine-editor/src/PaneHost.cpp
@@ -13,6 +13,51 @@ int PaneHost::GetPaneIndex(Pane* pane)
 	return -1;
 }
 
+int PaneHost::GetPaneIndex(int pane_id)
+{
+	for (int i = 0; i < (int)this->m_panes.size(); i++)
+	{
+		if (this->m_panes.at(i)->GetPaneID() == pane_id)
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+
+void PaneHost::AddPane(Pane* pane, int direction, bool docked)
+{
+	pane->SetPaneID(m_pane_id_counter);
+	pane->Bind(wxEVT_SIZE, &PaneHost::PaneResizeHandler, this);
+
+	m_pane_id_counter++;
+	this->m_panes.push_back(pane);
+
+	wxAuiPaneInfo info;
+	info = info.CaptionVisible(true);
+	info = info.Caption(pane->GetDisplayName());
+	info = info.Direction(direction);
+	info = info.Name(std::to_string(pane->GetPaneID()));
+
+	wxSize best_size = pane->GetBestSize();
+	info = info.MinSize(best_size);
+	info = info.FloatingSize(wxSize(best_size.x, best_size.y + 30)); //the header is included in this size. it appears to be 30px high on windows
+
+	this->m_panes_docked.push_back(docked);
+	if (docked)
+	{
+		info = info.Dock();
+	}
+	else
+	{
+		info = info.Float();
+	}
+
+	this->m_aui_manager.AddPane(pane, info);
+
+	this->m_aui_manager.Update();
+}
+
 PaneHost::PaneHost(Main* parent) : wxPanel(parent)
 {
 	this->m_parent = parent;
--- gameengine-editor/src/PaneHost.h
+++ gameengine-editor/src/PaneHost.h
@@ -20,6 +20,9 @@ private:
 	Main* m_parent;
 
 	int GetPaneIndex(Pane* pane);
+	int GetPaneIndex(int pane_id);
+	
+	void AddPane(Pane* pane, int direction, bool docked);
 
 public:
 	PaneHost(Main* parent);
@@ -41,30 +44,6 @@ inline void PaneHost::AddPane(int direction, bool docked)
 	static_assert(std::is_base_of<Pane, T>::value, "Supplied class must inherit from Pane");
 
 	Pane* new_pane = (Pane*)new T(this);
-	new_pane->SetPaneID(m_pane_id_counter);
-	new_pane->Bind(wxEVT_SIZE, &PaneHost::PaneResizeHandler, this);
-
-	m_pane_id_counter++;
-	this->m_panes.push_back(new_pane);
-
-	wxAuiPaneInfo info;
-	info = info.CaptionVisible(true);
-	info = info.MinSize(new_pane->GetBestSize());
-	info = info.Caption(new_pane->GetDisplayName());
-	info = info.Direction(direction);
-	info = info.Name(std::to_string(new_pane->GetPaneID()));
 	
-	this->m_panes_docked.push_back(docked);
-	if (docked)
-	{
-		info = info.Dock();
-	}
-	else
-	{
-		info = info.Float();
-	}
-	
-	this->m_aui_manager.AddPane(new_pane, info);
-
-	this->m_aui_manager.Update();
+	this->AddPane(new_pane, direction, docked);
 }
