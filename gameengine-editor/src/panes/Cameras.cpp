e1f154df721779a497883c519851c24a2ac81245
seckujdivad <juckes1david@gmail.com>
2020-06-29 05:15:00 +0100

    Added more controls to Cameras pane

--- gameengine-editor/src/panes/Cameras.cpp
+++ gameengine-editor/src/panes/Cameras.cpp
@@ -1,13 +1,32 @@
 #include "Cameras.h"
 
+void Cameras::evt_PosChanged(VectorCtrlEvent& evt)
+{
+	evt.Skip();
+}
+
 Cameras::Cameras(PaneHost* parent) : Pane(parent)
 {
 	this->m_sizer = new wxGridBagSizer(0, 0);
 
 	this->m_lb_cameras = new wxListBox(this, wxID_ANY);
-	this->m_sizer->Add(this->m_lb_cameras, wxGBPosition(0, 0), wxGBSpan(1, 1), wxEXPAND | wxALL);
+	this->m_sizer->Add(this->m_lb_cameras, wxGBPosition(0, 0), wxGBSpan(1, 2), wxEXPAND | wxALL);
 
-	this->m_sizer->AddGrowableCol(0);
+	this->m_stxt_fov = new wxStaticText(this, wxID_ANY, "FOV");
+	this->m_sizer->Add(this->m_stxt_fov, wxGBPosition(1, 0), wxGBSpan(1, 1), wxEXPAND | wxALL);
+
+	this->m_spndbl_fov = new wxSpinCtrlDouble(this, wxID_ANY);
+	this->m_spndbl_fov->SetValue(0);
+	this->m_sizer->Add(this->m_spndbl_fov, wxGBPosition(1, 1), wxGBSpan(1, 1), wxEXPAND | wxALL);
+
+	this->m_stxt_pos = new wxStaticText(this, wxID_ANY, "Position");
+	this->m_sizer->Add(this->m_stxt_pos, wxGBPosition(2, 0), wxGBSpan(1, 1), wxEXPAND | wxALL);
+
+	this->m_vct_pos = new VectorCtrl(this, wxID_ANY, 3);
+	this->m_vct_pos->Bind(EVT_VCTRCTRL_CHANGED, &Cameras::evt_PosChanged, this);
+	this->m_sizer->Add(this->m_vct_pos, wxGBPosition(2, 1), wxGBSpan(1, 1), wxEXPAND | wxALL);
+
+	this->m_sizer->AddGrowableCol(1);
 	this->m_sizer->AddGrowableRow(0);
 
 	this->SetSizer(this->m_sizer);
--- gameengine-editor/src/panes/Cameras.h
+++ gameengine-editor/src/panes/Cameras.h
@@ -6,13 +6,18 @@
 #include <wx/button.h>
 #include <wx/string.h>
 #include <wx/textctrl.h>
+#include <wx/spinctrl.h>
+#include <wx/stattext.h>
 
 #include <string>
+#include <vector>
 
 #include "scene/Scene.h"
+#include "scene/Camera.h"
 
 #include "Pane.h"
 #include "../PaneHost.h"
+#include "../VectorCtrl.h"
 
 class Cameras : public Pane
 {
@@ -20,6 +25,14 @@ private:
 	wxGridBagSizer* m_sizer;
 
 	wxListBox* m_lb_cameras;
+
+	wxStaticText* m_stxt_fov;
+	wxSpinCtrlDouble* m_spndbl_fov;
+
+	wxStaticText* m_stxt_pos;
+	VectorCtrl* m_vct_pos;
+
+	void evt_PosChanged(VectorCtrlEvent& evt);
 
 public:
 	Cameras(PaneHost* parent);
