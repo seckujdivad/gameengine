082042039e030100a114b8b4d2d8a19e98c73df7
seckujdivad <juckes1david@gmail.com>
2020-07-01 00:22:26 +0100

    Added all components to Cameras window and wrote loading of data from scene

--- gameengine-editor/src/panes/Cameras.cpp
+++ gameengine-editor/src/panes/Cameras.cpp
@@ -1,6 +1,55 @@
 #include "Cameras.h"
 
+void Cameras::evt_SelectionChanged(wxCommandEvent& evt)
+{
+	int index = this->m_lb_cameras->GetSelection();
+	if (index != -1)
+	{
+		std::string key = this->m_camera_names.at(index);
+		nlohmann::json data = this->GetPaneHost()->GetFileManager()->GetData()["cameras"][key];
+		
+		this->m_txt_name->SetValue(key);
+		this->m_vct_pos->SetValues({
+			data["position"][0].get<double>(),
+			data["position"][1].get<double>(),
+			data["position"][2].get<double>()
+			});
+		this->m_vct_rot->SetValues({
+			data["rotation"][0].get<double>(),
+			data["rotation"][1].get<double>(),
+			data["rotation"][2].get<double>()
+			});
+		this->m_vct_clips->SetValues({
+			data["clips"][0].get<double>(),
+			data["clips"][1].get<double>()
+			});
+		this->m_spndbl_fov->SetValue(data["fov"].get<double>());
+	}
+
+	evt.Skip();
+}
+
+void Cameras::evt_NameChanged(wxCommandEvent& evt)
+{
+	evt.Skip();
+}
+
+void Cameras::evt_FOVChanged(wxSpinDoubleEvent& evt)
+{
+	evt.Skip();
+}
+
 void Cameras::evt_PosChanged(VectorCtrlEvent& evt)
+{
+	evt.Skip();
+}
+
+void Cameras::evt_RotChanged(VectorCtrlEvent& evt)
+{
+	evt.Skip();
+}
+
+void Cameras::evt_ClipsChanged(VectorCtrlEvent& evt)
 {
 	evt.Skip();
 }
@@ -9,22 +58,48 @@ Cameras::Cameras(PaneHost* parent) : Pane(parent)
 {
 	this->m_sizer = new wxGridBagSizer(0, 0);
 
+	//create UI components
+	// list of camera names
 	this->m_lb_cameras = new wxListBox(this, wxID_ANY);
+	this->m_lb_cameras->Bind(wxEVT_LISTBOX, &Cameras::evt_SelectionChanged, this);
 	this->m_sizer->Add(this->m_lb_cameras, wxGBPosition(0, 0), wxGBSpan(1, 2), wxEXPAND | wxALL);
 
-	this->m_stxt_fov = new wxStaticText(this, wxID_ANY, "FOV");
-	this->m_sizer->Add(this->m_stxt_fov, wxGBPosition(1, 0), wxGBSpan(1, 1), wxEXPAND | wxALL);
+	// edit camera name
+	this->m_txt_name = new wxTextCtrl(this, wxID_ANY);
+	this->m_txt_name->Bind(wxEVT_TEXT, &Cameras::evt_NameChanged, this);
+	this->m_sizer->Add(this->m_txt_name, wxGBPosition(1, 0), wxGBSpan(1, 2), wxEXPAND | wxALL);
 
-	this->m_spndbl_fov = new wxSpinCtrlDouble(this, wxID_ANY);
-	this->m_spndbl_fov->SetValue(0);
-	this->m_sizer->Add(this->m_spndbl_fov, wxGBPosition(1, 1), wxGBSpan(1, 1), wxEXPAND | wxALL);
-
+	// edit position
 	this->m_stxt_pos = new wxStaticText(this, wxID_ANY, "Position");
 	this->m_sizer->Add(this->m_stxt_pos, wxGBPosition(2, 0), wxGBSpan(1, 1), wxEXPAND | wxALL);
 
 	this->m_vct_pos = new VectorCtrl(this, wxID_ANY, 3);
 	this->m_vct_pos->Bind(EVT_VCTRCTRL_CHANGED, &Cameras::evt_PosChanged, this);
 	this->m_sizer->Add(this->m_vct_pos, wxGBPosition(2, 1), wxGBSpan(1, 1), wxEXPAND | wxALL);
+
+	// edit rotation
+	this->m_stxt_rot = new wxStaticText(this, wxID_ANY, "Rotation");
+	this->m_sizer->Add(this->m_stxt_rot, wxGBPosition(3, 0), wxGBSpan(1, 1), wxEXPAND | wxALL);
+
+	this->m_vct_rot = new VectorCtrl(this, wxID_ANY, 3);
+	this->m_vct_rot->Bind(EVT_VCTRCTRL_CHANGED, &Cameras::evt_RotChanged, this);
+	this->m_sizer->Add(this->m_vct_rot, wxGBPosition(3, 1), wxGBSpan(1, 1), wxEXPAND | wxALL);
+
+	// edit fov
+	this->m_stxt_fov = new wxStaticText(this, wxID_ANY, "FOV");
+	this->m_sizer->Add(this->m_stxt_fov, wxGBPosition(4, 0), wxGBSpan(1, 1), wxEXPAND | wxALL);
+
+	this->m_spndbl_fov = new wxSpinCtrlDouble(this, wxID_ANY);
+	this->m_spndbl_fov->SetValue(0);
+	this->m_sizer->Add(this->m_spndbl_fov, wxGBPosition(4, 1), wxGBSpan(1, 1), wxEXPAND | wxALL);
+
+	// edit clips
+	this->m_stxt_clips = new wxStaticText(this, wxID_ANY, "Clips");
+	this->m_sizer->Add(this->m_stxt_clips, wxGBPosition(5, 0), wxGBSpan(1, 1), wxEXPAND | wxALL);
+
+	this->m_vct_clips = new VectorCtrl(this, wxID_ANY, 2);
+	this->m_vct_clips->Bind(EVT_VCTRCTRL_CHANGED, &Cameras::evt_ClipsChanged, this);
+	this->m_sizer->Add(this->m_vct_clips, wxGBPosition(5, 1), wxGBSpan(1, 1), wxEXPAND | wxALL);
 
 	this->m_sizer->AddGrowableCol(1);
 	this->m_sizer->AddGrowableRow(0);
@@ -42,8 +117,10 @@ std::string Cameras::GetDisplayName()
 void Cameras::SceneChangedEvent(Scene* scene)
 {
 	this->m_lb_cameras->Clear();
-	for (std::vector<Camera*>::iterator it = scene->cameras.begin(); it != scene->cameras.end(); it++)
+	this->m_camera_names.clear();
+	for (const auto& camera: this->GetPaneHost()->GetFileManager()->GetData()["cameras"].items())
 	{
-		this->m_lb_cameras->Append((*it)->GetIdentifier());
+		this->m_lb_cameras->Append(camera.key());
+		this->m_camera_names.push_back(camera.key());
 	}
 }
--- gameengine-editor/src/panes/Cameras.h
+++ gameengine-editor/src/panes/Cameras.h
@@ -26,13 +26,28 @@ private:
 
 	wxListBox* m_lb_cameras;
 
+	wxTextCtrl* m_txt_name;
+
 	wxStaticText* m_stxt_fov;
 	wxSpinCtrlDouble* m_spndbl_fov;
 
 	wxStaticText* m_stxt_pos;
 	VectorCtrl* m_vct_pos;
 
+	wxStaticText* m_stxt_rot;
+	VectorCtrl* m_vct_rot;
+
+	wxStaticText* m_stxt_clips;
+	VectorCtrl* m_vct_clips;
+
+	void evt_SelectionChanged(wxCommandEvent& evt);
+	void evt_NameChanged(wxCommandEvent& evt);
+	void evt_FOVChanged(wxSpinDoubleEvent& evt);
 	void evt_PosChanged(VectorCtrlEvent& evt);
+	void evt_RotChanged(VectorCtrlEvent& evt);
+	void evt_ClipsChanged(VectorCtrlEvent& evt);
+
+	std::vector<std::string> m_camera_names;
 
 public:
 	Cameras(PaneHost* parent);
