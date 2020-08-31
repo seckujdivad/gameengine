6f0edc8bee722b5ab2b6adfcf952341c19de34ec
seckujdivad <juckes1david@gmail.com>
2020-08-31 01:56:50 +0100

    RenderTexture now initialises target textures in debug mode with data (instead of just uninitialised memory)

--- gameengine/src/render/RenderTexture.cpp
+++ gameengine/src/render/RenderTexture.cpp
@@ -2,29 +2,117 @@
 
 void RenderTexture::CreateTextureData(GLuint& texture, GLenum type, GLenum internal_format, GLenum format, std::tuple<int, int> dimensions, GLint filtering, bool do_create)
 {
-	glGenTextures(1, &texture);
+	void* pixels = NULL;
+#ifdef _DEBUG
+	std::vector<GLubyte> pixel_data_ubyte(std::get<0>(dimensions) * std::get<1>(dimensions), 128);
+	std::vector<GLfloat> pixel_data_float(std::get<0>(dimensions) * std::get<1>(dimensions), 0.5f);
+
+	if (do_create)
+	{
+		if (format == GL_UNSIGNED_BYTE)
+		{
+			pixels = pixel_data_ubyte.data();
+		}
+		else if (format == GL_FLOAT)
+		{
+			pixels = pixel_data_float.data();
+		}
+		else
+		{
+			throw std::invalid_argument("Unknown format \"" + std::to_string(format) + "\"");
+		}
+	}
+#endif
+
+	if (do_create)
+	{
+		glGenTextures(1, &texture);
+	}
+
 	glBindTexture(type, texture);
-	glTexParameteri(type, GL_TEXTURE_MAG_FILTER, filtering);
-	glTexParameteri(type, GL_TEXTURE_MIN_FILTER, filtering);
-	glTexParameteri(type, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTexParameteri(type, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	glTexParameteri(type, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
 
 	if (type == GL_TEXTURE_2D)
 	{
-		glTexImage2D(GL_TEXTURE_2D, 0, internal_format, std::get<0>(dimensions), std::get<1>(dimensions), 0, internal_format, format, NULL);
+		glTexImage2D(GL_TEXTURE_2D, 0, internal_format, std::get<0>(dimensions), std::get<1>(dimensions), 0, internal_format, format, pixels);
 	}
 	else if (type == GL_TEXTURE_CUBE_MAP)
 	{
 		for (int i = 0; i < 6; i++)
 		{
-			glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, internal_format, std::get<0>(dimensions), std::get<1>(dimensions), 0, internal_format, format, NULL);
+			glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, internal_format, std::get<0>(dimensions), std::get<1>(dimensions), 0, internal_format, format, pixels);
 		}
 	}
 	else
 	{
 		throw std::runtime_error("Unknown texture type " + std::to_string(type));
 	}
+
+	glTexParameteri(type, GL_TEXTURE_MAG_FILTER, filtering);
+	glTexParameteri(type, GL_TEXTURE_MIN_FILTER, filtering);
+
+	glTexParameteri(type, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(type, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(type, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+	glTexParameteri(type, GL_TEXTURE_BASE_LEVEL, 0);
+	glTexParameteri(type, GL_TEXTURE_MAX_LEVEL, 0);
+
+#ifdef _DEBUG
+	if (do_create)
+	{
+		bool arrays_are_different = false;
+
+		if (format == GL_UNSIGNED_BYTE)
+		{
+			std::vector<GLubyte> return_data(std::get<0>(dimensions) * std::get<1>(dimensions));
+			glGetTexImage(type == GL_TEXTURE_CUBE_MAP ? GL_TEXTURE_CUBE_MAP_POSITIVE_X : type, 0, internal_format, format, return_data.data());
+
+			if (return_data.size() == pixel_data_ubyte.size())
+			{
+				for (std::size_t i = 0; i < return_data.size(); i++)
+				{
+					if (std::abs(return_data.at(i) - pixel_data_ubyte.at(i)) > 0)
+					{
+						arrays_are_different = true;
+					}
+				}
+			}
+			else
+			{
+				arrays_are_different = true;
+			}
+		}
+		else if (format == GL_FLOAT)
+		{
+			std::vector<GLfloat> return_data(std::get<0>(dimensions) * std::get<1>(dimensions));
+			glGetTexImage(type == GL_TEXTURE_CUBE_MAP ? GL_TEXTURE_CUBE_MAP_POSITIVE_X : type, 0, internal_format, format, return_data.data());
+			
+			if (return_data.size() == pixel_data_float.size())
+			{
+				for (std::size_t i = 0; i < return_data.size(); i++)
+				{
+					if (std::abs(return_data.at(i) - pixel_data_float.at(i)) > 0.000001f)
+					{
+						arrays_are_different = true;
+					}
+				}
+			}
+			else
+			{
+				arrays_are_different = true;
+			}
+		}
+		else
+		{
+			throw std::invalid_argument("Unknown format \"" + std::to_string(format) + "\"");
+		}
+
+		if (arrays_are_different)
+		{
+			throw std::runtime_error("Texture is not using the set data");
+		}
+	}
+#endif
 }
 
 void RenderTexture::InitialiseTextureGroup(RenderTextureGroup& texture_group, int num_data_tex, GLenum type, bool do_create)
