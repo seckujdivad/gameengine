a2f4faf7b6d502a56e8750deeba14b8aa5072c16
seckujdivad <juckes1david@gmail.com>
2020-09-02 20:18:43 +0100

    Optimised compilation of  GLComponents header by moving stdexcept from the header file into the source file

--- gameengine/src/GLComponents.cpp
+++ gameengine/src/GLComponents.cpp
@@ -1,5 +1,7 @@
 #include "GLComponents.h"
 
+#include <stdexcept>
+
 void GL_CHECK_ERROR()
 {
 	GLenum exception = glGetError();
--- gameengine/src/GLComponents.h
+++ gameengine/src/GLComponents.h
@@ -3,8 +3,6 @@
 #include <GL/glew.h>
 #include <wx/glcanvas.h>
 
-#include <stdexcept>
-
 /*
 Using this header (instead of including any of the headers included in this header) prevents GL/gl.h being included before GL/glew.h (which is required by GLEW).
 */
