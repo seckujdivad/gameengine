6e09e84b48aad1bd802d3a5452f383fc0efd8ac0
seckujdivad <juckes1david@gmail.com>
2020-10-15 11:49:58 +0100

    Fixed model wireframe tests

--- gameengine-GTest/src/scene/model/ModelTests.cpp
+++ gameengine-GTest/src/scene/model/ModelTests.cpp
@@ -1,12 +1,27 @@
 #include "pch.h"
-
 #include "scene/model/Model.h"
 
 TEST(ModelTests, WireframeColour_SetGet)
 {
 	Model* mdl = new Model(0, ModelGeometry());
 
-	mdl->SetWireframeColour(glm::vec3(0.0f, 0.5f, 1.0f));
+	mdl->SetWireframeColours({
+		glm::vec3(0.0f),
+		glm::vec3(1.0f),
+		glm::vec3(1.0f, 0.5f, 0.25f)
+		});
 
-	EXPECT_EQ(mdl->GetWireframeColour(), glm::vec3(0.0f, 0.5f, 1.0f));
+	EXPECT_EQ(mdl->GetCurrentWireframeColour(), glm::vec3(0.0f));
+	
+	mdl->SetCurrentWireframeColour(glm::vec3(0.0f, 1.0f, 0.0f));
+	EXPECT_EQ(mdl->GetCurrentWireframeColour(), glm::vec3(0.0f, 1.0f, 0.0f));
+
+	mdl->SetCurrentWireframeIndex(1);
+	EXPECT_EQ(mdl->GetCurrentWireframeColour(), glm::vec3(1.0f));
+
+	mdl->SetCurrentWireframeIndex(2);
+	EXPECT_EQ(mdl->GetCurrentWireframeColour(), glm::vec3(1.0f, 0.5f, 0.25f));
+
+	mdl->SetCurrentWireframeIndex(0);
+	EXPECT_EQ(mdl->GetCurrentWireframeColour(), glm::vec3(0.0f, 1.0f, 0.0f));
 }
