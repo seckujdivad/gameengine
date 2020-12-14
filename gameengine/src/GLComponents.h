4d19b5e0bca57e5c6f1147ab0a7afdbfd5a23f89
seckujdivad <juckes1david@gmail.com>
2020-12-14 04:08:12 +0000

    Changed GL_CHECK_ERROR to be more reusable

--- gameengine/src/GLComponents.h
+++ gameengine/src/GLComponents.h
@@ -3,13 +3,15 @@
 #include <GL/glew.h>
 #include <wx/glcanvas.h>
 
+#include <string>
+
 /*
 Using this header (instead of including any of the headers included in this header) prevents GL/gl.h being included before GL/glew.h (which is required by GLEW).
 */
 
 const int GAMEENGINE_NUM_DATA_TEX = 1;
 
-void GL_CHECK_ERROR();
+std::string GL_CHECK_ERROR();
 
 /*
 * Ideally wxWidgets should be handling all platform-specific OpenGL API calls.
