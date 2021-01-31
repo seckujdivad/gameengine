28266dfcc1c15b58a1c250faff6ab199e9e29881
seckujdivad <juckes1david@gmail.com>
2021-01-31 01:40:59 +0000

    Update NormalRenderer.cpp

--- gameengine/src/render/renderer/NormalRenderer.cpp
+++ gameengine/src/render/renderer/NormalRenderer.cpp
@@ -19,18 +19,16 @@ NormalRenderer::NormalRenderer(Engine* engine, RenderTarget* target) : Renderer(
 		throw std::invalid_argument("Target must inherit from RenderTexture");
 	}
 
-	{
-		RenderTextureInfo info;
-		info.colour = false;
-		info.num_data = 0;
-		info.auto_generate_textures = false;
+	RenderTextureInfo info;
+	info.colour = false;
+	info.num_data = 0;
+	info.auto_generate_textures = false;
 
-		RenderTargetConfig config;
-		config.SetMode(RenderTargetMode::Normal_DepthOnly);
+	RenderTargetConfig config;
+	config.SetMode(RenderTargetMode::Normal_DepthOnly);
 
-		this->m_rt_depth_only = std::make_unique<RenderTexture>(-1, this->GetEngine(), config, info, this->GetTarget()->GetTargetType());
-		this->m_rt_depth_only->SetWriteTarget(target_texture);
-	}
+	this->m_rt_depth_only = std::make_unique<RenderTexture>(-1, this->GetEngine(), config, info, this->GetTarget()->GetTargetType());
+	this->m_rt_depth_only->SetWriteTarget(target_texture);
 }
 
 bool NormalRenderer::SetOutputSize(std::tuple<int, int> dimensions)
