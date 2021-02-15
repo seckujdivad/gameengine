0ed1ce234ddad60349541f232104bd88193497f2
seckujdivad <juckes1david@gmail.com>
2021-02-15 18:17:23 +0000

    Engine now stores Textures instead of LocalTextures for 2D static textures

--- gameengine/src/render/LoadedTexture.h
+++ gameengine/src/render/LoadedTexture.h
@@ -1,11 +1,29 @@
 #pragma once
 
 #include <string>
+#include <memory>
 
 #include "../GLComponents.h"
 
+#include "texture/Texture.h"
+#include "TargetType.h"
+
 struct LoadedTexture
 {
+	inline LoadedTexture() {};
+	inline LoadedTexture(std::shared_ptr<Texture> texture)
+	{
+		*this = texture;
+	};
+
+	inline LoadedTexture& operator=(const std::shared_ptr<Texture>& texture)
+	{
+		this->id = texture->GetTexture();
+		this->type = GetTargetEnum(texture->GetTargetType());
+
+		return *this;
+	}
+
 	GLuint id = NULL;
 	GLenum type = NULL;
 	std::string uniform_name;
