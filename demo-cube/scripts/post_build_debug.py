3d376eb68fd710d5d02240cc859d482f32b87743
seckujdivad <juckes1david@gmail.com>
2021-05-20 03:26:25 +0100

    Disabled automatic rebuilding of server for debug builds

--- demo-cube/scripts/post_build_debug.py
+++ demo-cube/scripts/post_build_debug.py
@@ -4,7 +4,7 @@ import subprocess
 
 import post_build
 
-server_dir = os.path.abspath(os.path.join(sys.path[0], "..", "..", "gameengine-server"))
-post_build.build(server_dir, False)
+#server_dir = os.path.abspath(os.path.join(sys.path[0], "..", "..", "gameengine-server"))
+#post_build.build(server_dir, False)
 
-post_build.copy_server(os.path.abspath(os.path.join(sys.path[0], "..", "..")))
+#post_build.copy_server(os.path.abspath(os.path.join(sys.path[0], "..", "..")))
