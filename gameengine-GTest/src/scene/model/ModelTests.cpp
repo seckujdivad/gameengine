5eef12685e778f89b160f65a19b2ac76dc9ffe31
seckujdivad <juckes1david@gmail.com>
2021-07-15 15:04:40 +0100

    Fixed Model test

--- gameengine-GTest/src/scene/model/ModelTests.cpp
+++ gameengine-GTest/src/scene/model/ModelTests.cpp
@@ -3,33 +3,34 @@
 #include <vector>
 #include <memory>
 
+#include "scene/Scene.h"
 #include "scene/model/Model.h"
 #include "scene/model/geometry/Polygonal.h"
 
 TEST(ModelTests, WireframeColour_SetGet)
 {
-	std::vector<std::shared_ptr<Geometry>> geometry({ std::make_shared<Polygonal>() });
-	Model* mdl = new Model(0, geometry);
+	Scene scene;
 
-	mdl->SetWireframeColours({
+	std::vector<std::shared_ptr<Geometry>> geometry({ std::make_shared<Polygonal>() });
+	Model mdl = Model(0, geometry, &scene);
+
+	mdl.SetWireframeColours({
 		glm::vec3(0.0f),
 		glm::vec3(1.0f),
 		glm::vec3(1.0f, 0.5f, 0.25f)
 		});
 
-	EXPECT_EQ(mdl->GetCurrentWireframeColour(), glm::vec3(0.0f));
+	EXPECT_EQ(mdl.GetCurrentWireframeColour(), glm::vec3(0.0f));
 	
-	mdl->SetCurrentWireframeColour(glm::vec3(0.0f, 1.0f, 0.0f));
-	EXPECT_EQ(mdl->GetCurrentWireframeColour(), glm::vec3(0.0f, 1.0f, 0.0f));
+	mdl.SetCurrentWireframeColour(glm::vec3(0.0f, 1.0f, 0.0f));
+	EXPECT_EQ(mdl.GetCurrentWireframeColour(), glm::vec3(0.0f, 1.0f, 0.0f));
 
-	mdl->SetCurrentWireframeIndex(1);
-	EXPECT_EQ(mdl->GetCurrentWireframeColour(), glm::vec3(1.0f));
+	mdl.SetCurrentWireframeIndex(1);
+	EXPECT_EQ(mdl.GetCurrentWireframeColour(), glm::vec3(1.0f));
 
-	mdl->SetCurrentWireframeIndex(2);
-	EXPECT_EQ(mdl->GetCurrentWireframeColour(), glm::vec3(1.0f, 0.5f, 0.25f));
+	mdl.SetCurrentWireframeIndex(2);
+	EXPECT_EQ(mdl.GetCurrentWireframeColour(), glm::vec3(1.0f, 0.5f, 0.25f));
 
-	mdl->SetCurrentWireframeIndex(0);
-	EXPECT_EQ(mdl->GetCurrentWireframeColour(), glm::vec3(0.0f, 1.0f, 0.0f));
-
-	delete mdl;
+	mdl.SetCurrentWireframeIndex(0);
+	EXPECT_EQ(mdl.GetCurrentWireframeColour(), glm::vec3(0.0f, 1.0f, 0.0f));
 }
