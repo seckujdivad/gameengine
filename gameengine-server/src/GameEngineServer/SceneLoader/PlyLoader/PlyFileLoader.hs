3d015c2cb5bdcc8285d074016881da81cec1a648
seckujdivad <juckes1david@gmail.com>
2021-08-06 01:18:44 +0100

    Wrote parsing of values in body of PLY files

--- gameengine-server/src/GameEngineServer/SceneLoader/PlyLoader/PlyFileLoader.hs
+++ gameengine-server/src/GameEngineServer/SceneLoader/PlyLoader/PlyFileLoader.hs
@@ -2,36 +2,37 @@ module GameEngineServer.SceneLoader.PlyLoader.PlyFileLoader (generatePLYFile, PL
 
 import Data.Maybe (mapMaybe)
 
-import Data.Map (Map, empty, update, lookup, keys)
+import Data.Map (Map, empty, update, lookup, keys, fromList, insert)
 
-import Data.ByteString.Lazy (ByteString)
+import qualified Data.ByteString.Lazy as LBS (ByteString, null, fromStrict, fromStrict, toStrict)
+import Data.ByteString.Lex.Fractional (readDecimal, readSigned)
 
-import GameEngineServer.SceneLoader.PlyLoader.PlyParser (ParseError, Parse (..), ElementType (..))
+import qualified GameEngineServer.SceneLoader.PlyLoader.PlyParser as PlyParser (ParseError, Parse (..), ElementType (..))
 
     
 -- |Generates 'Geometry' from a parsed PLY file in the form of a list of 'Parse' with line indices
-generatePLYFile :: [(Int, Parse)] -> Either PLYFile PLYFileGenerationError
+generatePLYFile :: [(Int, PlyParser.Parse)] -> Either PLYFile PLYFileGenerationError
 generatePLYFile parses = case null parseErrors of
         True -> generatePLYFileInner parses initialState
         False -> Right $ ParsingErrors parseErrors
     where
         parseErrors = mapMaybe (\(lineNumber, parse) -> case parse of
-            Malformed parseError -> Just (lineNumber, parseError)
+            PlyParser.Malformed parseError -> Just (lineNumber, parseError)
             _ -> Nothing) parses
 
-generatePLYFileInner :: [(Int, Parse)] -> GeometryGeneratorState -> Either PLYFile PLYFileGenerationError
+generatePLYFileInner :: [(Int, PlyParser.Parse)] -> GeometryGeneratorState -> Either PLYFile PLYFileGenerationError
 generatePLYFileInner [] state = case state of
     EndOfFile plyFile -> Left $ plyFile
     _ -> Right $ PLYFileGenerationError Nothing UnexpectedEOF
 generatePLYFileInner ((lineNumber, parse):remainingParses) state = case state of
     Preamble fileTypeSeen formatSeen -> case parse of
-        FileType -> if fileTypeSeen then
+        PlyParser.FileType -> if fileTypeSeen then
                 generateError $ BadPreamble "FileType appeared twice in the preamble"
             else (if formatSeen then
                     generateError $ BadPreamble "Format was seen before FileType"
                 else
                     doNextParse (Preamble True formatSeen))
-        Format isCorrect -> if (not fileTypeSeen) || formatSeen then
+        PlyParser.Format isCorrect -> if (not fileTypeSeen) || formatSeen then
                 generateError $ BadPreamble $ (if not fileTypeSeen then "Format appeared before FileType in the preamble" else "") ++ (if not fileTypeSeen && formatSeen then ". " else "") ++ (if formatSeen then "Format appeared twice in the preamble" else "")
             else (if isCorrect then
                     doNextParse (Header [])
@@ -40,26 +41,38 @@ generatePLYFileInner ((lineNumber, parse):remainingParses) state = case state of
         _ ->  generateError $ BadPreamble "Preamble must only contain FileType and Format"
     
     Header headerElements -> case parse of
-        Element name count -> if count >= 0 then
-                doNextParse $ Header (headerElements ++ [HeaderElement name (fromIntegral count) []])
-            else
-                generateError $ BadHeader "Negative numbers of elements can't be provided"
+        PlyParser.Element name count -> case count >= 0 of
+            True -> doNextParse $ Header (headerElements ++ [HeaderElement name (fromIntegral count) []])
+            False -> generateError $ BadHeader "Negative numbers of elements can't be provided"
 
-        Property name propType -> doNextParse $ Header $ modifyLast (\(HeaderElement elementName count properties) -> HeaderElement elementName count (properties ++ [HeaderValueProperty name (elementTypeToHeaderValue propType)])) headerElements
+        PlyParser.Property name propType -> doNextParse $ Header $ modifyLast (\(HeaderElement elementName count properties) -> HeaderElement elementName count (properties ++ [HeaderValueProperty name (elementTypeToHeaderValue propType)])) headerElements
 
-        GameEngineServer.SceneLoader.PlyLoader.PlyParser.ListProperty name sizeType propType -> case elementTypeToHeaderValue sizeType of
-            HeaderIntegerValue -> doNextParse $ Header $ modifyLast (\(HeaderElement elementName count properties) -> HeaderElement elementName count (properties ++ [HeaderValueProperty name (elementTypeToHeaderValue propType)])) headerElements
+        PlyParser.ListProperty name sizeType propType -> case elementTypeToHeaderValue sizeType of
+            HeaderIntegerValue -> doNextParse $ Header $ modifyLast (\(HeaderElement elementName count properties) -> HeaderElement elementName count (properties ++ [HeaderListProperty name (elementTypeToHeaderValue propType)])) headerElements
             HeaderDoubleValue -> generateError $ BadHeader "ListProperty size type must be an integer variant"
 
-        EndHeader -> doNextParse $ GameEngineServer.SceneLoader.PlyLoader.PlyFileLoader.Body headerElements 0 0 emptyPLYFile
+        PlyParser.EndHeader -> doNextParse $ Body headerElements 0 emptyPLYFile
 
         _ -> generateError $ BadHeader "Header must only contain Element, Property, ListProperty or EndHeader"
     
-    GameEngineServer.SceneLoader.PlyLoader.PlyFileLoader.Body headerElements currentElementIndex numOfElementProcessed (PLYFile elementMap) -> case parse of
-        GameEngineServer.SceneLoader.PlyLoader.PlyParser.Body byteStrings -> generateError $ ExpectedEOF -- TODO
+    Body (headerElement:headerElements) numOfElementProcessed (PLYFile elementMap) -> case parse of
+        PlyParser.Body byteStrings -> case loadElement lineNumber headerElement byteStrings of
+                Left element -> case moveToNextElement of
+                        True -> doNextParse $ Body (headerElement:headerElements) (numOfElementProcessed + 1) newPLyFile
+                        False -> case null headerElements of
+                            True -> doNextParse $ EndOfFile newPLyFile
+                            False -> doNextParse $ Body headerElements 0 newPLyFile
+                    where
+                        newPLyFile = PLYFile (insert elementName element elementMap)
+                        moveToNextElement = numOfElementProcessed + 1 >= elementCount
+                
+                Right error -> Right error
             where
-                currentElement = headerElements !! currentElementIndex
+                HeaderElement elementName elementCount _ = headerElement
+        
         _ -> generateError $ BadBody "Only Body is allowed in body"
+    
+    Body [] _ _ -> generateError $ BadBody "Invalid state - shouldn't be in Body state with no header elements still to be processed"
     
     EndOfFile _ -> generateError $ ExpectedEOF
 
@@ -69,7 +82,7 @@ generatePLYFileInner ((lineNumber, parse):remainingParses) state = case state of
         
 
 data PLYFileGenerationError =
-    ParsingErrors [(Int, ParseError)]
+    ParsingErrors [(Int, PlyParser.ParseError)]
     | PLYFileGenerationError (Maybe Int) PLYFileGenerationErrorInner
 
 instance Show PLYFileGenerationError where
@@ -89,33 +102,33 @@ data PLYFileGenerationErrorInner =
     | BadHeader String
     | BadBody String
     | ExpectedEOF
+    | ValueUnparseable String
     deriving (Show)
 
 data GeometryGeneratorState =
     -- |Preamble of the PLY file. First 'Bool' is whether or not 'FileType' has been seen, second is the same but for 'Format'
     Preamble Bool Bool
     | Header [HeaderElement]
-    | Body [HeaderElement] Int Int PLYFile
+    | Body [HeaderElement] Int PLYFile
     -- |The end of the file is expected
     | EndOfFile PLYFile
 
 initialState :: GeometryGeneratorState
 initialState = Preamble False False
 
-data HeaderElement = HeaderElement ByteString Int [HeaderProperty]
-data HeaderProperty = HeaderValueProperty ByteString HeaderValue | HeaderListProperty ByteString [HeaderValue]
+data HeaderElement = HeaderElement LBS.ByteString Int [HeaderProperty]
+data HeaderProperty = HeaderValueProperty LBS.ByteString HeaderValue | HeaderListProperty LBS.ByteString HeaderValue
 data HeaderValue = HeaderDoubleValue | HeaderIntegerValue
 
-elementTypeToHeaderValue :: ElementType -> HeaderValue
+elementTypeToHeaderValue :: PlyParser.ElementType -> HeaderValue
 elementTypeToHeaderValue elementType = case elementType of
-    FloatingPoint -> HeaderDoubleValue
-    Integer -> HeaderIntegerValue
-    UnsignedInteger -> HeaderIntegerValue
+    PlyParser.FloatingPoint -> HeaderDoubleValue
+    PlyParser.Integer -> HeaderIntegerValue
+    PlyParser.UnsignedInteger -> HeaderIntegerValue
 
-
-data PLYFile = PLYFile (Map ByteString Element)
-data Element = Map ByteString Property
-data Property = ValueProperty ByteString Value | ListProperty ByteString [Value]
+data PLYFile = PLYFile (Map LBS.ByteString Element)
+data Element = Element (Map LBS.ByteString Property)
+data Property = ValueProperty Value | ListProperty [Value]
 data Value = DoubleValue Double | IntegerValue Integer
 
 emptyPLYFile :: PLYFile
@@ -123,3 +136,68 @@ emptyPLYFile = PLYFile empty
 
 modifyLast :: (a -> a) -> [a] -> [a]
 modifyLast f xs = (init xs) ++ [f (last xs)]
+
+loadElement :: Int -> HeaderElement -> [LBS.ByteString] -> Either Element PLYFileGenerationError
+loadElement lineNumber (HeaderElement name _ properties) bodyByteStrings = case loadedElementByteStringOrError of
+        Left (element, remainingByteString) -> case null remainingByteString of
+            True -> Left $ element
+            False -> Right $ PLYFileGenerationError (Just lineNumber) $ BadBody "Too many values/not enough properties"
+        Right error -> Right error
+    where
+        loadedElementByteStringOrError = foldl (loadElementInner lineNumber) (Left $ (Element empty, bodyByteStrings)) properties
+
+
+loadElementInner :: Int -> Either (Element, [LBS.ByteString]) PLYFileGenerationError -> HeaderProperty -> Either (Element, [LBS.ByteString]) PLYFileGenerationError
+loadElementInner _ (Right error) _ = Right error
+loadElementInner lineNumber (Left (element, byteStrings)) headerProperty = case headerProperty of
+        HeaderValueProperty name valueType -> case null byteStrings of
+                True -> generateError $ BadBody "Can't read value property - no more values left"
+                False -> case loadValue lineNumber valueType (head byteStrings) of
+                    Left value -> Left $ (Element $ insert name (ValueProperty value) propertyMap, tail byteStrings)
+                    Right error -> Right error
+            where
+                Element propertyMap = element
+        HeaderListProperty name valueType -> case null byteStrings of
+            True -> generateError $ BadBody "Can't read size of list - no more values"
+            False -> case loadValue lineNumber HeaderIntegerValue $ head byteStrings of
+                Left (IntegerValue listSize) -> case length byteStrings >= (intListSize + 1) of
+                        True -> case null errors of
+                                True -> Left $ (Element $ insert name (ListProperty values) propertyMap, remainingByteStrings)
+                                    where
+                                        Element propertyMap = element
+                                        remainingByteStrings = drop (intListSize + 1) byteStrings
+
+                                        values = mapMaybe (\valueOrError -> case valueOrError of
+                                            Left value -> Just value
+                                            Right error -> Nothing
+                                            ) valuesOrErrors
+
+                                False -> Right $ head errors
+                            where
+                                valuesOrErrors = map (loadValue lineNumber valueType) (take intListSize (tail byteStrings))
+
+                                errors = mapMaybe (\valueOrError -> case valueOrError of
+                                    Left value -> Nothing
+                                    Right error -> Just error
+                                    ) valuesOrErrors
+
+                        False -> generateError $ BadBody $ "List size is " ++ show listSize ++ ", but there is/are only " ++ show (length byteStrings - 1) ++ " value(s) left"
+                    where
+                        intListSize = fromIntegral listSize
+                Left (DoubleValue _) -> Prelude.error "loadValue returned double when the type was specified as integer"
+                Right error -> Right error
+    where
+        generateError error = Right $ PLYFileGenerationError (Just lineNumber) error
+
+loadValue :: Int -> HeaderValue -> LBS.ByteString -> Either Value PLYFileGenerationError
+loadValue lineNumber HeaderDoubleValue byteString = case (readSigned readDecimal) $ LBS.toStrict byteString of
+    Just (value, remainingByteString) -> case LBS.null $ LBS.fromStrict remainingByteString of
+        True -> Left $ DoubleValue value
+        False -> Right $ PLYFileGenerationError (Just lineNumber) $ ValueUnparseable "Bytes were left over after parsing value"
+    Nothing -> Right $ PLYFileGenerationError (Just lineNumber) $ ValueUnparseable "Couldn't parse value"
+
+loadValue lineNumber HeaderIntegerValue byteString = case (readSigned readDecimal) $ LBS.toStrict byteString of
+    Just (value, remainingByteString) -> case LBS.null $ LBS.fromStrict remainingByteString of
+        True -> Left $ IntegerValue $ round value
+        False -> Right $ PLYFileGenerationError (Just lineNumber) $ ValueUnparseable "Bytes were left over after parsing value"
+    Nothing -> Right $ PLYFileGenerationError (Just lineNumber) $ ValueUnparseable "Couldn't parse value"
