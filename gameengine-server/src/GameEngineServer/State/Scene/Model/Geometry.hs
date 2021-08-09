5f470d739e303b68e80737489ecd6cb74a7e7877
seckujdivad <juckes1david@gmail.com>
2021-08-09 03:53:47 +0100

    Added invertNormals function

--- gameengine-server/src/GameEngineServer/State/Scene/Model/Geometry.hs
+++ gameengine-server/src/GameEngineServer/State/Scene/Model/Geometry.hs
@@ -1,14 +1,16 @@
-module GameEngineServer.State.Scene.Model.Geometry (Geometry (..), Face (..)) where
+module GameEngineServer.State.Scene.Model.Geometry (Geometry (..), Face (..), invertNormals) where
 
 import Linear.V3 (V3)
 
 
+-- |A data type for storing geometry
 data Geometry =
     -- |Geometry made up of 'Polygon's
     Polygonal [Face]
 
-
--- Polygonal
+-- |Invert the normals of a 'Geometry'
+invertNormals :: Geometry -> Geometry
+invertNormals (Polygonal faces) = Polygonal (map (\face -> face {faceNormal = 0 - faceNormal face}) faces)
 
 -- |Represents one face of a 'Polygonal'
 data Face =
