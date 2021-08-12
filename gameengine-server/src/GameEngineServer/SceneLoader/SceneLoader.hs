385c452c04e0821dba5b4e192b2c61afec09915d
seckujdivad <juckes1david@gmail.com>
2021-08-12 20:11:16 +0100

    Wrote loading of Scene

--- gameengine-server/src/GameEngineServer/SceneLoader/SceneLoader.hs
+++ gameengine-server/src/GameEngineServer/SceneLoader/SceneLoader.hs
@@ -1,12 +1,49 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module GameEngineServer.SceneLoader.SceneLoader () where
+module GameEngineServer.SceneLoader.SceneLoader (loadScene) where
 
-import Data.Aeson (FromJSON, parseJSON, (.:), withObject)
+import Prelude hiding (readFile)
+
+import Data.Aeson (FromJSON, parseJSON, (.:), withObject, decode, withArray)
+
+import System.FilePath ((</>))
+
+import Data.ByteString.Lazy (readFile)
+
+import Data.Vector (toList)
+
+import Data.Maybe (mapMaybe)
 
 import GameEngineServer.State.Scene.Scene (Scene (..))
 import GameEngineServer.SceneLoader.ModelLoader (UnloadedModel (..), loadModel)
+import GameEngineServer.SceneLoader.GeometryLoader (GeometryInfoTable (..), createGeometryTable)
 
 
---instance FromJSON Scene where
---    parseJSON = withObject "" $ \obj -> Scene []
+-- |Contains all the information taken from a scene file required to load a 'Scene'
+data UnloadedScene =
+    -- |Contains all the information taken from a scene file required to load a 'Scene'
+    UnloadedScene [UnloadedModel] GeometryInfoTable
+
+instance FromJSON UnloadedScene where
+    parseJSON = withObject "UnloadedScene" $ \obj -> do
+        modelConfigs <- obj .: "layout"
+        modelValues <- withArray "UnloadedScene layout" (return . toList) modelConfigs
+        models <- foldr (\value modelsParser -> do
+            models <- modelsParser
+            model <- parseJSON value
+            return (model:models)) (return []) modelValues
+        
+        geometryInfoTable <- obj .: "models"
+
+        return $ UnloadedScene models geometryInfoTable
+
+-- |Load a 'Scene' from the disk given the root path and relative path to the scene file
+loadScene :: FilePath -> FilePath -> IO (Maybe Scene)
+loadScene rootPath configPath = do
+    sceneConfig <- readFile $ rootPath </> configPath
+    case decode sceneConfig of
+        Just unloadedScene -> do
+            let UnloadedScene unloadedModels geometryInfoTable = unloadedScene
+            geometryTable <- createGeometryTable geometryInfoTable rootPath
+            return $ Just $ Scene (mapMaybe (loadModel geometryTable) unloadedModels)
+        Nothing -> return Nothing
