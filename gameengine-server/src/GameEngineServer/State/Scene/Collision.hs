35fe72fadb019b9fc248a7b53079eca468e5e626
seckujdivad <juckes1david@gmail.com>
2021-10-04 00:06:42 +0100

    Wrote rigid body collision processing

--- gameengine-server/src/GameEngineServer/State/Scene/Collision.hs
+++ gameengine-server/src/GameEngineServer/State/Scene/Collision.hs
@@ -1,21 +1,50 @@
 module GameEngineServer.State.Scene.Collision (Collision (..), collide) where
 
-import Linear.V3 (V3)
+import Linear.V3 (V3 (V3))
+import Linear.Metric (distance)
 
-import GameEngineServer.State.Scene.Motion (Motion (..))
+import GameEngineServer.State.Scene.Motion (Motion (..), motionFromVelocityAndMomentOrMass, motionFromMomentumAndMomentOrMass)
 
 
+-- |Describes the motion of two particles (which are nominally involved in some kind of collision)
 type MotionPair a = (Motion a, Motion a)
 
+-- |Describes a Newtonian collision between two particles
 data Collision a =
+    -- |Describes a Newtonian collision between two particles
     Collision {
+        -- |Motion of the particles
         collMotion :: MotionPair a,
+        -- |Coefficient of restitution of the collision
         collRestitutionCoefficient :: a
     }
 
 -- |Process a collision between two objects
-collide :: Num a => Collision a -> (V3 a, V3 a) -> MotionPair a
-collide (Collision (firstMotion, secondMotion) restitutionCoefficient) (firstResultantUnitDirection, secondResultantUnitDirection) = (firstMotion, secondMotion)
+collide :: (Num a, Floating a) => Collision a -> (V3 a, V3 a) -> MotionPair a
+collide (Collision (firstMotion, secondMotion) restitutionCoefficient) (_, secondResultantUnitDirection) = (resultantFirstMotion, resultantSecondMotion)
     where
+        -- momentum and kinetic energy are conserved (minus the coefficient of restitution)
         resultantMomentum = mtnMomentum firstMotion + mtnMomentum secondMotion
         resultantKEnergy = restitutionCoefficient * (mtnKEnergy firstMotion + mtnKEnergy secondMotion)
+
+        absoluteResultantMomentum = distance (V3 0 0 0) resultantMomentum
+
+        -- solve second particle absolute resultant velocity as part of the system of simultaneous equations
+        -- WARNING - likely failure point
+        secondAbsoluteResultantVelocityNumeratorConstant = 2 * (mtnMomentOrMass secondMotion) * absoluteResultantMomentum
+        secondAbsoluteResultantVelocityNumeratorIrrational = 2 * mtnMomentOrMass firstMotion * mtnMomentOrMass secondMotion * ((2 * absoluteResultantMomentum * absoluteResultantMomentum) - (resultantKEnergy * (mtnMomentOrMass firstMotion + mtnMomentOrMass secondMotion)))
+
+        secondAbsoluteResultantVelocityDenominator = 2 * (mtnMomentOrMass secondMotion) * ((mtnMomentOrMass firstMotion) + (mtnMomentOrMass secondMotion))
+
+        secondAbsoluteResultantVelocity = (secondAbsoluteResultantVelocityNumeratorConstant + (sqrt secondAbsoluteResultantVelocityNumeratorIrrational)) / secondAbsoluteResultantVelocityDenominator
+
+        secondResultantVelocity = (return secondAbsoluteResultantVelocity) * secondResultantUnitDirection
+
+        -- solve rest of second particle
+        secondResultantMomentum = (return $ mtnMomentOrMass secondMotion) * secondResultantVelocity
+
+        resultantSecondMotion = motionFromVelocityAndMomentOrMass secondResultantVelocity (mtnMomentOrMass secondMotion)
+
+        -- find first particle from second particle
+        firstResultantMomentum = resultantMomentum - secondResultantMomentum
+        resultantFirstMotion = motionFromMomentumAndMomentOrMass firstResultantMomentum (mtnMomentOrMass firstMotion)
--- gameengine-server/src/GameEngineServer/State/Scene/Motion.hs
+++ gameengine-server/src/GameEngineServer/State/Scene/Motion.hs
@@ -1,6 +1,7 @@
-module GameEngineServer.State.Scene.Motion (Motion (..), mtnVelocity) where
+module GameEngineServer.State.Scene.Motion (Motion (..), mtnVelocity, motionFromVelocityAndMomentOrMass, motionFromMomentumAndMomentOrMass) where
 
 import Linear.V3 (V3)
+import Linear.Metric (distance)
 
 
 -- |Describes either translational or rotational motion
@@ -21,3 +22,18 @@ mtnVelocity motion = (mtnMomentum motion) / (return $ mtnMomentOrMass motion)
 
 instance Show a => Show (Motion a) where
     show (Motion kinEnergy momentum momentOrMass) = "Motion - kinetic energy: " ++ show kinEnergy ++ ", momentum: " ++ show momentum ++ ", moment of inertia/mass: " ++ show momentOrMass
+
+-- |Construct a 'Motion' from a given velocity and moment of inertia/mass
+motionFromVelocityAndMomentOrMass :: Floating a => V3 a -> a -> Motion a
+motionFromVelocityAndMomentOrMass velocity momentOrMass = Motion kineticEnergy momentum momentOrMass
+    where
+        velocityScalar = distance (return 0) velocity
+        kineticEnergy = 0.5 * momentOrMass * velocityScalar * velocityScalar
+        momentum = velocity * (return momentOrMass)
+
+-- |Construct a 'Motion' from a given momentum and moment of inertia/mass
+motionFromMomentumAndMomentOrMass :: Floating a => V3 a -> a -> Motion a
+motionFromMomentumAndMomentOrMass momentum momentOrMass = Motion kineticEnergy momentum momentOrMass
+    where
+        velocityScalar = (distance (return 0) momentum) / momentOrMass
+        kineticEnergy = 0.5 * momentOrMass * velocityScalar * velocityScalar
